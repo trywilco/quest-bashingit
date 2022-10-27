d6c596b3fada12f397ca457b3a93ef2cf81125f1
Shem <smagnezi8@gmail.com>
2022-10-27 13:20:11 +0300

    Update check.js

--- tests/check.js
+++ tests/check.js
@@ -7,7 +7,7 @@ echo "4632a06f-c142-4e64-a8cc-2102bbf1de30,Scala,Kitti,kscala81@boston.com,854,I
 echo "04bbd023-c977-45ec-98d1-047af2d63b11,Mapson,Amazon,emapson82@jiathis.com,150,Sweden" >> input.txt
 echo "bd530b55-1d2e-480b-b924-fd91c358b0d3,MacDowall,Arnie,amacdowall83@Amazon.com,866,Australia" >> input.txt
 
-./scripts/extract_names.sh input.txt output_names.txt
+./backend/scripts/extract_names.sh input.txt output_names.txt
 
 if ! grep -q Last output_names.txt; then
   exit 1;
